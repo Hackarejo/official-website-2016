055f384f84a8d8c0ff97eda6ae5028e4cd61df42
Ivo Batistela <irbatistela@gmail.com>
2016-03-27 22:25:52 -0300

    leaflet setup

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,25 @@ app.config(function() {
 	new WOW().init();
 });
 
-app.controller('main-controller', function() {
+app.controller('main-controller', function($scope) {
+	var hackLat = -26.069740;
+	var hackLng = -53.043451;
 
+	angular.extend($scope, {
+		citfb: {
+			lat: hackLat, 
+			lng: hackLng,
+			zoom: 18
+		},
+
+		marker: {
+			mainMarker: {
+				lat: hackLat,
+				lng: hackLng,
+				focus: true,
+				message: "CITFBE - Hackarejo 2016",
+				draggable: false
+			}
+		}
+	});
 });
